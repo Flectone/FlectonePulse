81fd6adae8455fd8a845e69d77b595df8d375cab
TheFaser <artem.mochalin11@gmail.com>
2025-01-03 16:46:16 +0500

    fix item builder for older versions

--- bukkit/src/main/java/net/flectone/pulse/module/command/chatsetting/BukkitChatsettingModule.java
+++ bukkit/src/main/java/net/flectone/pulse/module/command/chatsetting/BukkitChatsettingModule.java
@@ -1,5 +1,7 @@
 package net.flectone.pulse.module.command.chatsetting;
 
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import com.github.retrooper.packetevents.protocol.component.ComponentTypes;
 import com.github.retrooper.packetevents.protocol.component.builtin.item.ItemLore;
 import com.github.retrooper.packetevents.protocol.item.ItemStack;
@@ -9,13 +11,15 @@ import io.github.retrooper.packetevents.util.SpigotConversionUtil;
 import net.flectone.pulse.file.Command;
 import net.flectone.pulse.manager.FileManager;
 import net.flectone.pulse.manager.InventoryManager;
-import net.flectone.pulse.util.PermissionUtil;
 import net.flectone.pulse.manager.ThreadManager;
 import net.flectone.pulse.model.FPlayer;
 import net.flectone.pulse.module.command.FCommand;
 import net.flectone.pulse.util.ComponentUtil;
+import net.flectone.pulse.util.PermissionUtil;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Material;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +42,11 @@ public class BukkitChatsettingModule extends ChatsettingModule {
 
     @Override
     public ItemStack buildItemStack(int settingIndex, FPlayer fPlayer, List<String> itemMessages, Command.Chatsetting.SettingItem settingItem) {
-        Component name = itemMessages.isEmpty() ? Component.empty() : componentUtil.builder(fPlayer, itemMessages.get(0)).build();
+
+        Component name = itemMessages.isEmpty()
+                ? Component.empty()
+                : componentUtil.builder(fPlayer, itemMessages.get(0)).build();
+
         List<Component> lore = new ArrayList<>();
         if (itemMessages.size() > 1) {
             itemMessages.stream()
@@ -51,11 +59,26 @@ public class BukkitChatsettingModule extends ChatsettingModule {
                     );
         }
 
-        return new ItemStack.Builder()
-                .type(SpigotConversionUtil.fromBukkitItemMaterial(Material.valueOf(settingItem.getMaterials().get(settingIndex))))
-                .component(ComponentTypes.ITEM_NAME, name)
-                .component(ComponentTypes.LORE, new ItemLore(lore))
-                .build();
+        if (PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_20_5)) {
+            return new ItemStack.Builder()
+                    .type(SpigotConversionUtil.fromBukkitItemMaterial(Material.valueOf(settingItem.getMaterials().get(settingIndex))))
+                    .component(ComponentTypes.ITEM_NAME, name)
+                    .component(ComponentTypes.LORE, new ItemLore(lore))
+                    .build();
+        }
+
+        org.bukkit.inventory.ItemStack legacyItemStack = new org.bukkit.inventory.ItemStack(Material.valueOf(settingItem.getMaterials().get(settingIndex)));
+
+        ItemMeta itemMeta = legacyItemStack.getItemMeta();
+        itemMeta.setDisplayName(LegacyComponentSerializer.legacySection().serialize(name));
+        itemMeta.setLore(lore.stream()
+                .map(component -> LegacyComponentSerializer.legacySection().serialize(component))
+                .toList()
+        );
+
+        legacyItemStack.setItemMeta(itemMeta);
+
+        return SpigotConversionUtil.fromBukkitItemStack(legacyItemStack);
     }
 
     @Override
