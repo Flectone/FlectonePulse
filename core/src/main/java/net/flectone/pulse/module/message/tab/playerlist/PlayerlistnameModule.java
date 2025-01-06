c86f1f803f418107bff048d729a57ec49be66404
TheFaser <artem.mochalin11@gmail.com>
2025-01-06 20:48:24 +0500

    add legacy playerlistname update

--- core/src/main/java/net/flectone/pulse/module/message/tab/playerlist/PlayerlistnameModule.java
+++ core/src/main/java/net/flectone/pulse/module/message/tab/playerlist/PlayerlistnameModule.java
@@ -1,5 +1,8 @@
 package net.flectone.pulse.module.message.tab.playerlist;
 
+import com.github.retrooper.packetevents.PacketEvents;
+import com.github.retrooper.packetevents.manager.server.ServerVersion;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerInfo;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerInfoUpdate;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -65,27 +68,43 @@ public class PlayerlistnameModule extends AbstractModuleMessage<Localization.Mes
     public void send(FPlayer fPlayer) {
         if (checkModulePredicates(fPlayer)) return;
 
-        fPlayerManager.getFPlayers().stream().filter(FPlayer::isOnline).forEach(fReceiver -> {
-
-            Component message = componentUtil.builder(fPlayer, fReceiver, resolveLocalization(fReceiver).getFormat())
-                    .userMessage(false)
-                    .build();
-
-            WrapperPlayServerPlayerInfoUpdate.PlayerInfo info = new WrapperPlayServerPlayerInfoUpdate.PlayerInfo(
-                    packetEventsUtil.getUser(fPlayer).getProfile(),
-                    true,
-                    fPlayerManager.getPing(fPlayer),
-                    fPlayerManager.getGamemode(fPlayer),
-                    message,
-                    null
-            );
-
-            packetEventsUtil.sendPacket(fReceiver, new WrapperPlayServerPlayerInfoUpdate(WrapperPlayServerPlayerInfoUpdate.Action.UPDATE_DISPLAY_NAME, info));
-        });
+        fPlayerManager.getFPlayers().stream()
+                .filter(FPlayer::isOnline)
+                .forEach(fReceiver -> updatePlayerlistname(fPlayer, fReceiver));
     }
 
     @Override
     public boolean isConfigEnable() {
         return message.isEnable();
+    }
+
+    private void updatePlayerlistname(FPlayer fPlayer, FPlayer fReceiver) {
+        Component name = componentUtil.builder(fPlayer, fReceiver, resolveLocalization(fReceiver).getFormat())
+                .userMessage(false)
+                .build();
+
+        if (PacketEvents.getAPI().getServerManager().getVersion().isNewerThanOrEquals(ServerVersion.V_1_19_4)) {
+            WrapperPlayServerPlayerInfoUpdate.PlayerInfo playerInfo = new WrapperPlayServerPlayerInfoUpdate.PlayerInfo(
+                    packetEventsUtil.getUser(fPlayer).getProfile(),
+                    true,
+                    fPlayerManager.getPing(fPlayer),
+                    fPlayerManager.getGamemode(fPlayer),
+                    name,
+                    null
+            );
+
+            packetEventsUtil.sendPacket(fReceiver, new WrapperPlayServerPlayerInfoUpdate(WrapperPlayServerPlayerInfoUpdate.Action.UPDATE_DISPLAY_NAME, playerInfo));
+            return;
+        }
+
+
+        WrapperPlayServerPlayerInfo.PlayerData playerData = new WrapperPlayServerPlayerInfo.PlayerData(
+                name,
+                packetEventsUtil.getUser(fPlayer).getProfile(),
+                fPlayerManager.getGamemode(fPlayer),
+                fPlayerManager.getPing(fPlayer)
+        );
+
+        packetEventsUtil.sendPacket(fReceiver, new WrapperPlayServerPlayerInfo(WrapperPlayServerPlayerInfo.Action.UPDATE_DISPLAY_NAME, playerData));
     }
 }
