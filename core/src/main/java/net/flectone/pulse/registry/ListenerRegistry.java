43c22ae0a1b6dc7b4c402733ac344b6e0da28e83
TheFaser <artem.mochalin11@gmail.com>
2025-04-26 19:36:52 +0500

    feat(ListenerRegistry): add singleton and add constructor injection

--- core/src/main/java/net/flectone/pulse/registry/ListenerRegistry.java
+++ core/src/main/java/net/flectone/pulse/registry/ListenerRegistry.java
@@ -3,7 +3,9 @@ package net.flectone.pulse.registry;
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketListenerCommon;
 import com.github.retrooper.packetevents.event.PacketListenerPriority;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.google.inject.Singleton;
 import net.flectone.pulse.listener.AbstractPacketListener;
 import net.flectone.pulse.listener.BasePacketListener;
 import net.flectone.pulse.listener.InventoryPacketListener;
@@ -12,12 +14,14 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class ListenerRegistry {
+@Singleton
+public class ListenerRegistry {
 
     private final List<PacketListenerCommon> packetListeners = new ArrayList<>();
 
     private final Injector injector;
 
+    @Inject
     public ListenerRegistry(Injector injector) {
         this.injector = injector;
     }
