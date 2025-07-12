55566ab9eb0346f58ac6ab0b7210686056261f15
TheFaser <artem.mochalin11@gmail.com>
2025-07-12 23:06:34 +0500

    refactor: rename Team class to model for scoreboard message format

--- core/src/main/java/net/flectone/pulse/module/message/format/scoreboard/model/Team.java
+++ core/src/main/java/net/flectone/pulse/module/message/format/scoreboard/model/Team.java
@@ -1,4 +1,4 @@
-package net.flectone.pulse.module.message.format.scoreboard;
+package net.flectone.pulse.module.message.format.scoreboard.model;
 
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerTeams;
 
