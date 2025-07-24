46f21578b2bd0fe907e822d741a841300b8c2d2b
TheFaser <artem.mochalin11@gmail.com>
2025-07-24 13:37:32 +0500

    refactor: update TimeFormatter to use Apache Commons Lang 3

--- core/src/main/java/net/flectone/pulse/formatter/TimeFormatter.java
+++ core/src/main/java/net/flectone/pulse/formatter/TimeFormatter.java
@@ -5,7 +5,7 @@ import com.google.inject.Singleton;
 import net.flectone.pulse.configuration.Localization;
 import net.flectone.pulse.resolver.FileResolver;
 import net.flectone.pulse.model.FPlayer;
-import org.apache.commons.lang.time.DurationFormatUtils;
+import org.apache.commons.lang3.time.DurationFormatUtils;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
