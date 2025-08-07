63223b0007884e721c272fb8293dfefc73d55480
TheFaser <artem.mochalin11@gmail.com>
2025-08-08 01:29:48 +0500

    refactor: add conflict check between InteractiveChat and Delete module

--- bukkit/src/main/java/net/flectone/pulse/module/integration/interactivechat/InteractiveChatModule.java
+++ bukkit/src/main/java/net/flectone/pulse/module/integration/interactivechat/InteractiveChatModule.java
@@ -3,33 +3,44 @@ package net.flectone.pulse.module.integration.interactivechat;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import net.flectone.pulse.config.Integration;
+import net.flectone.pulse.config.Message;
 import net.flectone.pulse.config.Permission;
 import net.flectone.pulse.model.entity.FEntity;
 import net.flectone.pulse.module.AbstractModule;
 import net.flectone.pulse.platform.registry.ListenerRegistry;
 import net.flectone.pulse.processing.resolver.FileResolver;
+import net.flectone.pulse.util.logging.FLogger;
 import net.kyori.adventure.text.Component;
 
 @Singleton
 public class InteractiveChatModule extends AbstractModule {
 
+    private final Message.Format.Moderation.Delete deleteMessage;
     private final Integration.Interactivechat integration;
     private final Permission.Integration.Interactivechat permission;
     private final InteractiveChatIntegration interactiveChatIntegration;
     private final ListenerRegistry listenerRegistry;
+    private final FLogger fLogger;
 
     @Inject
     public InteractiveChatModule(FileResolver fileResolver,
                                  InteractiveChatIntegration interactiveChatIntegration,
-                                 ListenerRegistry listenerRegistry) {
+                                 ListenerRegistry listenerRegistry,
+                                 FLogger fLogger) {
+        this.deleteMessage = fileResolver.getMessage().getFormat().getModeration().getDelete();
         this.integration = fileResolver.getIntegration().getInteractivechat();
         this.permission = fileResolver.getPermission().getIntegration().getInteractivechat();
         this.interactiveChatIntegration = interactiveChatIntegration;
         this.listenerRegistry = listenerRegistry;
+        this.fLogger = fLogger;
     }
 
     @Override
     public void onEnable() {
+        if (deleteMessage.isEnable()) {
+            fLogger.warning("InteractiveChat and Delete module incompatible");
+        }
+
         registerModulePermission(permission);
 
         interactiveChatIntegration.hook();
