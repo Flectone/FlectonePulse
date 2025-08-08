eac5a7823d8e726e29f6093fd2982e8428317f84
TheFaser <artem.mochalin11@gmail.com>
2025-08-08 10:23:52 +0500

    refactor: remove unused code in FColor class statements

--- core/src/main/java/net/flectone/pulse/model/FColor.java
+++ core/src/main/java/net/flectone/pulse/model/FColor.java
@@ -1,19 +1,10 @@
 package net.flectone.pulse.model;
 
-import java.util.Arrays;
-import java.util.Optional;
-
 public record FColor(int number, String name) {
 
     public enum Type {
         SEE, // always first
-        OUT; // always second
-
-        public static Optional<Type> fromString(String string) {
-            return Arrays.stream(Type.values())
-                    .filter(type -> type.name().equalsIgnoreCase(string))
-                    .findFirst();
-        }
+        OUT // always second
     }
 
 }
