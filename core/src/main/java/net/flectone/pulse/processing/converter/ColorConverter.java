03b45cb97ab63ef29c6c4d15ed8f1a4ba9ccfafd
TheFaser <artem.mochalin11@gmail.com>
2025-08-11 00:32:49 +0500

    feat(chatcolor): add support for legacy and named colors and allow any input for admins

--- core/src/main/java/net/flectone/pulse/processing/converter/ColorConverter.java
+++ core/src/main/java/net/flectone/pulse/processing/converter/ColorConverter.java
@@ -4,11 +4,10 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import lombok.Getter;
 import net.flectone.pulse.util.logging.FLogger;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @Singleton
@@ -16,82 +15,25 @@ public class ColorConverter {
 
     private final int defaultBackground = 0x40000000;
 
-    private final Map<String, String> minecraftHexMap = new HashMap<>();
-    private final Map<String, String> legacyHexMap = new HashMap<>();
-
-    private final List<String> minecraftList = List.of(
-            "black", "dark_blue", "dark_green",
-            "dark_aqua", "dark_red", "dark_purple",
-            "gold", "gray", "dark_gray",
-            "blue", "green", "aqua",
-            "red", "light_purple", "yellow",
-            "white", "light_blue", "lime",
-            "cyan", "magenta", "orange",
-            "brown", "pink", "purple",
-            "silver"
+    private final List<String> legacyColors = List.of(
+            "&0", "&1", "&2", "&3", "&4", "&5", "&6", "&7", "&8", "&9",
+            "&a", "&b", "&c", "&d", "&e", "&f", "&k", "&l", "&m", "&n", "&o"
     );
 
-    private final List<String> hexSymbolList = List.of(
-            "0", "1", "2",
-            "3", "4", "5",
-            "6", "7", "8",
-            "9", "a", "b",
-            "c", "d", "e",
-            "f"
-    );
+    private final List<String> namedColors = NamedTextColor.NAMES.values()
+            .stream()
+            .map(namedTextColor -> "<" + namedTextColor.toString() + ">")
+            .toList();
 
     private final FLogger fLogger;
 
     @Inject
     public ColorConverter(FLogger fLogger) {
         this.fLogger = fLogger;
-
-        minecraftHexMap.put("black", "#000000");
-        minecraftHexMap.put("dark_blue", "#0000AA");
-        minecraftHexMap.put("dark_green", "#00AA00");
-        minecraftHexMap.put("dark_aqua", "#00AAAA");
-        minecraftHexMap.put("dark_red", "#AA0000");
-        minecraftHexMap.put("dark_purple", "#AA00AA");
-        minecraftHexMap.put("gold", "#FFAA00");
-        minecraftHexMap.put("gray", "#AAAAAA");
-        minecraftHexMap.put("dark_gray", "#555555");
-        minecraftHexMap.put("blue", "#5555FF");
-        minecraftHexMap.put("green", "#55FF55");
-        minecraftHexMap.put("aqua", "#55FFFF");
-        minecraftHexMap.put("red", "#FF5555");
-        minecraftHexMap.put("light_purple", "#FF55FF");
-        minecraftHexMap.put("yellow", "#FFFF55");
-        minecraftHexMap.put("white", "#FFFFFF");
-        minecraftHexMap.put("light_blue", "#55FFFF");
-        minecraftHexMap.put("lime", "#55FF55");
-        minecraftHexMap.put("cyan", "#55FFFF");
-        minecraftHexMap.put("magenta", "#FF55FF");
-        minecraftHexMap.put("orange", "#FFAA00");
-        minecraftHexMap.put("brown", "#A52A2A");
-        minecraftHexMap.put("pink", "#FFC0CB");
-        minecraftHexMap.put("purple", "#800080");
-        minecraftHexMap.put("silver", "#C0C0C0");
-
-        legacyHexMap.put("&0", "#000000");
-        legacyHexMap.put("&1", "#0000AA");
-        legacyHexMap.put("&2", "#00AA00");
-        legacyHexMap.put("&3", "#00AAAA");
-        legacyHexMap.put("&4", "#AA0000");
-        legacyHexMap.put("&5", "#AA00AA");
-        legacyHexMap.put("&6", "#FFAA00");
-        legacyHexMap.put("&7", "#AAAAAA");
-        legacyHexMap.put("&8", "#555555");
-        legacyHexMap.put("&9", "#5555FF");
-        legacyHexMap.put("&a", "#55FF55");
-        legacyHexMap.put("&b", "#55FFFF");
-        legacyHexMap.put("&c", "#FF5555");
-        legacyHexMap.put("&d", "#FF55FF");
-        legacyHexMap.put("&e", "#FFFF55");
-        legacyHexMap.put("&f", "#FFFFFF");
     }
 
     @Nullable
-    public String convert(String color) {
+    public String isCorrect(String color) {
         if (color == null) return null;
 
         if (isHex(color)) {
@@ -105,11 +47,15 @@ public class ColorConverter {
             }
         }
 
-        if (color.startsWith("&")) {
-            return legacyHexMap.getOrDefault(color, null);
+        if (legacyColors.contains(color)) {
+            return color;
         }
 
-        return minecraftHexMap.getOrDefault(color, null);
+        if (namedColors.contains(color)) {
+            return color;
+        }
+
+        return null;
     }
 
     private boolean isHex(String color) {
