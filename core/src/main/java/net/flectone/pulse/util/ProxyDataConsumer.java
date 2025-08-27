ef1c2538f1c0665999960b78392e67a80455b1ae
TheFaser <artem.mochalin11@gmail.com>
2025-08-27 21:28:42 +0500

    refactor: remove unused BiProxyDataConsumer interface

--- core/src/main/java/net/flectone/pulse/util/ProxyDataConsumer.java
+++ core/src/main/java/net/flectone/pulse/util/ProxyDataConsumer.java
@@ -3,7 +3,7 @@ package net.flectone.pulse.util;
 import java.io.IOException;
 
 @FunctionalInterface
-public interface ProxyDataConsumer<T>  {
+public interface ProxyDataConsumer<T> {
 
     void accept(T t) throws IOException;
 
