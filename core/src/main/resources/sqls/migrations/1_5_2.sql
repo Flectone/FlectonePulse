a2dc4fcd3c9368047372ef5af81c96ed6223f253
TheFaser <artem.mochalin11@gmail.com>
2025-09-05 15:01:44 +0500

    refactor: add valid column to mail and ignore tables

--- core/src/main/resources/sqls/migrations/1_5_2.sql
+++ core/src/main/resources/sqls/migrations/1_5_2.sql
@@ -1,3 +1,7 @@
+ALTER TABLE `mail` ADD COLUMN `valid` INTEGER NOT NULL DEFAULT 1;
+
+ALTER TABLE `ignore` ADD COLUMN `valid` INTEGER NOT NULL DEFAULT 1;
+
 UPDATE `setting` SET `type` = 'CHAT_NAME' WHERE `type` = 'CHAT';
 
 UPDATE `setting` SET `type` = 'SPY_STATUS' WHERE `type` = 'SPY';
