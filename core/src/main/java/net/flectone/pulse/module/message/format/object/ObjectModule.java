5db387aa9bc46e3589a6092c35f8c72ac9812f63
TheFaser <artem.mochalin11@gmail.com>
2025-10-08 18:42:49 +0500

    fix(object): checks for player head and sprite tags

--- core/src/main/java/net/flectone/pulse/module/message/format/object/ObjectModule.java
+++ core/src/main/java/net/flectone/pulse/module/message/format/object/ObjectModule.java
@@ -74,6 +74,7 @@ public class ObjectModule extends AbstractModule {
     public void addPlayerHeadTag(MessageContext messageContext) {
         FEntity sender = messageContext.getSender();
         if (isModuleDisabledFor(sender)) return;
+        if (!config().isPlayerHead()) return;
         if (!permissionChecker.check(sender, permission().getPlayerHead())) return;
 
         messageContext.addReplacementTag(MessagePipeline.ReplacementTag.PLAYER_HEAD, ((argumentQueue, context) -> {
@@ -113,6 +114,7 @@ public class ObjectModule extends AbstractModule {
     public void addSpriteTag(MessageContext messageContext) {
         FEntity sender = messageContext.getSender();
         if (isModuleDisabledFor(sender)) return;
+        if (!config().isSprite()) return;
         if (!permissionChecker.check(sender, permission().getSprite())) return;
 
         messageContext.addReplacementTag(MessagePipeline.ReplacementTag.SPRITE, ((argumentQueue, context) -> {
