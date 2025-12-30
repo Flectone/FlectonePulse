63e334db233a5342f3a2e487105898f681bb607d
TheFaser <artem.mochalin11@gmail.com>
2025-12-30 19:47:48 +0500

    refactor: move setup brigadier to abstract registry

--- bukkit/src/main/java/net/flectone/pulse/platform/registry/ModernBukkitCommandRegistry.java
+++ bukkit/src/main/java/net/flectone/pulse/platform/registry/ModernBukkitCommandRegistry.java
@@ -2,23 +2,13 @@ package net.flectone.pulse.platform.registry;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import io.leangen.geantyref.TypeToken;
 import net.flectone.pulse.execution.scheduler.TaskScheduler;
-import net.flectone.pulse.model.entity.FPlayer;
 import net.flectone.pulse.platform.handler.CommandExceptionHandler;
 import net.flectone.pulse.processing.mapper.FPlayerMapper;
-import net.flectone.pulse.processing.parser.integer.ColorParser;
-import net.flectone.pulse.processing.parser.integer.DurationReasonParser;
-import net.flectone.pulse.processing.parser.player.PlayerParser;
-import net.flectone.pulse.processing.parser.string.MessageParser;
-import net.flectone.pulse.util.file.FileFacade;
 import net.flectone.pulse.processing.resolver.ReflectionResolver;
+import net.flectone.pulse.util.file.FileFacade;
 import org.bukkit.plugin.Plugin;
-import org.incendo.cloud.brigadier.BrigadierSetting;
-import org.incendo.cloud.brigadier.CloudBrigadierManager;
 import org.incendo.cloud.bukkit.CloudBukkitCapabilities;
-import org.incendo.cloud.parser.standard.StringParser;
 
 @Singleton
 public class ModernBukkitCommandRegistry extends LegacyBukkitCommandRegistry {
@@ -34,31 +24,7 @@ public class ModernBukkitCommandRegistry extends LegacyBukkitCommandRegistry {
 
         if (manager.hasCapability(CloudBukkitCapabilities.NATIVE_BRIGADIER)) {
             manager.registerBrigadier();
-            CloudBrigadierManager<FPlayer, ?> brigadierManager = manager.brigadierManager();
-            brigadierManager.setNativeSuggestions(new TypeToken<StringParser<FPlayer>>() {}, true);
-
-            brigadierManager.registerMapping(new TypeToken<PlayerParser>() {},
-                    builder -> builder.cloudSuggestions()
-                            .to(argument -> StringArgumentType.string())
-            );
-
-            brigadierManager.registerMapping(new TypeToken<DurationReasonParser>() {},
-                    builder -> builder.cloudSuggestions()
-                            .to(argument -> StringArgumentType.greedyString())
-            );
-
-            brigadierManager.registerMapping(new TypeToken<ColorParser>() {},
-                    builder -> builder.cloudSuggestions()
-                            .to(argument -> StringArgumentType.greedyString())
-            );
-
-            brigadierManager.registerMapping(new TypeToken<MessageParser>() {},
-                    builder -> builder.cloudSuggestions()
-                            .to(argument -> StringArgumentType.greedyString())
-            );
-
-            brigadierManager.settings().set(BrigadierSetting.FORCE_EXECUTABLE, true);
-
+            setupBrigadierManager(manager.brigadierManager());
         } else if (manager.hasCapability(CloudBukkitCapabilities.ASYNCHRONOUS_COMPLETION)) {
             manager.registerAsynchronousCompletions();
         }
--- core/src/main/java/net/flectone/pulse/platform/registry/CommandRegistry.java
+++ core/src/main/java/net/flectone/pulse/platform/registry/CommandRegistry.java
@@ -1,8 +1,17 @@
 package net.flectone.pulse.platform.registry;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
+import io.leangen.geantyref.TypeToken;
 import net.flectone.pulse.model.entity.FPlayer;
+import net.flectone.pulse.processing.parser.integer.ColorParser;
+import net.flectone.pulse.processing.parser.integer.DurationReasonParser;
+import net.flectone.pulse.processing.parser.player.PlayerParser;
+import net.flectone.pulse.processing.parser.string.MessageParser;
 import org.incendo.cloud.Command;
 import org.incendo.cloud.CommandManager;
+import org.incendo.cloud.brigadier.BrigadierSetting;
+import org.incendo.cloud.brigadier.CloudBrigadierManager;
+import org.incendo.cloud.parser.standard.StringParser;
 
 import java.util.function.Function;
 
@@ -14,5 +23,32 @@ public abstract class CommandRegistry implements Registry {
     public abstract void registerCommand(Function<CommandManager<FPlayer> , Command.Builder<FPlayer>> builder);
 
     public abstract void unregisterCommand(String name);
+
+
+    public void setupBrigadierManager(CloudBrigadierManager<FPlayer, ?> brigadierManager) {
+        brigadierManager.setNativeSuggestions(new TypeToken<StringParser<FPlayer>>() {}, true);
+
+        brigadierManager.registerMapping(new TypeToken<PlayerParser>() {},
+                builder -> builder.cloudSuggestions()
+                        .to(argument -> StringArgumentType.string())
+        );
+
+        brigadierManager.registerMapping(new TypeToken<DurationReasonParser>() {},
+                builder -> builder.cloudSuggestions()
+                        .to(argument -> StringArgumentType.greedyString())
+        );
+
+        brigadierManager.registerMapping(new TypeToken<ColorParser>() {},
+                builder -> builder.cloudSuggestions()
+                        .to(argument -> StringArgumentType.greedyString())
+        );
+
+        brigadierManager.registerMapping(new TypeToken<MessageParser>() {},
+                builder -> builder.cloudSuggestions()
+                        .to(argument -> StringArgumentType.greedyString())
+        );
+
+        brigadierManager.settings().set(BrigadierSetting.FORCE_EXECUTABLE, true);
+    }
 
 }
