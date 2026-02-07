1db0a89dcf2d1a1ef7466d584ad0a6be77c685ef
TheFaser <artem.mochalin11@gmail.com>
2026-02-07 14:13:44 +0500

    feat(integration): add PlaceholderAPI integration for Hytale server

--- hytale/src/main/java/net/flectone/pulse/module/integration/HytaleIntegrationModule.java
+++ hytale/src/main/java/net/flectone/pulse/module/integration/HytaleIntegrationModule.java
@@ -1,23 +1,42 @@
 package net.flectone.pulse.module.integration;
 
+import com.google.common.collect.ImmutableList;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
 import net.flectone.pulse.model.entity.FEntity;
 import net.flectone.pulse.model.entity.FPlayer;
 import net.flectone.pulse.model.util.ExternalModeration;
+import net.flectone.pulse.module.AbstractModule;
+import net.flectone.pulse.module.integration.placeholderapi.HytalePlaceholderAPIModule;
 import net.flectone.pulse.platform.adapter.PlatformServerAdapter;
 import net.flectone.pulse.util.file.FileFacade;
 import net.kyori.adventure.text.Component;
+import org.jspecify.annotations.NonNull;
 
 @Singleton
 public class HytaleIntegrationModule extends IntegrationModule {
+
+    private final PlatformServerAdapter platformServerAdapter;
 
     @Inject
     public HytaleIntegrationModule(FileFacade fileFacade,
                                    PlatformServerAdapter platformServerAdapter,
                                    Injector injector) {
         super(fileFacade, platformServerAdapter, injector);
+
+        this.platformServerAdapter = platformServerAdapter;
+    }
+
+    @Override
+    public ImmutableList.Builder<@NonNull Class<? extends AbstractModule>> childrenBuilder() {
+        ImmutableList.Builder<@NonNull Class<? extends AbstractModule>> builder = super.childrenBuilder();
+
+        if (platformServerAdapter.hasProject("HelpChat:PlaceholderAPI")) {
+            builder.add(HytalePlaceholderAPIModule.class);
+        }
+
+        return builder;
     }
 
     @Override
---
+++ hytale/src/main/java/net/flectone/pulse/module/integration/placeholderapi/HytalePlaceholderAPIIntegration.java
@@ -0,0 +1,174 @@
+package net.flectone.pulse.module.integration.placeholderapi;
+
+import at.helpch.placeholderapi.PlaceholderAPI;
+import at.helpch.placeholderapi.expansion.PlaceholderExpansion;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.Universe;
+import lombok.RequiredArgsConstructor;
+import net.flectone.pulse.BuildConfig;
+import net.flectone.pulse.annotation.Pulse;
+import net.flectone.pulse.execution.scheduler.TaskScheduler;
+import net.flectone.pulse.listener.PulseListener;
+import net.flectone.pulse.model.FColor;
+import net.flectone.pulse.model.entity.FEntity;
+import net.flectone.pulse.model.entity.FPlayer;
+import net.flectone.pulse.model.event.Event;
+import net.flectone.pulse.model.event.message.MessageFormattingEvent;
+import net.flectone.pulse.module.command.mute.MuteModule;
+import net.flectone.pulse.module.integration.FIntegration;
+import net.flectone.pulse.platform.adapter.PlatformServerAdapter;
+import net.flectone.pulse.processing.context.MessageContext;
+import net.flectone.pulse.service.FPlayerService;
+import net.flectone.pulse.util.checker.PermissionChecker;
+import net.flectone.pulse.util.constant.MessageFlag;
+import net.flectone.pulse.util.constant.MessageType;
+import net.flectone.pulse.util.constant.SettingText;
+import net.flectone.pulse.util.file.FileFacade;
+import net.flectone.pulse.util.logging.FLogger;
+import org.apache.commons.lang3.StringUtils;
+import org.jspecify.annotations.NonNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Singleton
+@RequiredArgsConstructor(onConstructor = @__(@Inject))
+public class HytalePlaceholderAPIIntegration extends PlaceholderExpansion implements FIntegration, PulseListener {
+
+    private final FileFacade fileFacade;
+    private final FPlayerService fPlayerService;
+    private final PlatformServerAdapter platformServerAdapter;
+    private final PermissionChecker permissionChecker;
+    private final HytalePlaceholderAPIModule placeholderAPIModule;
+    private final TaskScheduler taskScheduler;
+    private final MuteModule muteModule;
+    private final FLogger fLogger;
+
+    @Override
+    public @NonNull String getIdentifier() {
+        return BuildConfig.PROJECT_NAME;
+    }
+
+    @Override
+    public @NonNull String getAuthor() {
+        return BuildConfig.PROJECT_AUTHOR;
+    }
+
+    @Override
+    public @NonNull String getVersion() {
+        return BuildConfig.PROJECT_VERSION;
+    }
+
+    @Override
+    public void hook() {
+        // idk why PlaceholderAPI can't register even though FlectonePulse sees it
+        taskScheduler.runSyncLater(this::register, 20L);
+        fLogger.info("✔ PlaceholderAPI hooked");
+    }
+
+    @Override
+    public void unhook() {
+        taskScheduler.runSync(this::unregister);
+        fLogger.info("✖ PlaceholderAPI unhooked");
+    }
+
+    @Override
+    public String onPlaceholderRequest(PlayerRef player, @NonNull String params) {
+        if (player == null) return null;
+
+        FPlayer fPlayer = fPlayerService.getFPlayer(player.getUuid());
+
+        params = params.toLowerCase();
+        if (params.equalsIgnoreCase("mute_suffix")) {
+            return muteModule.getMuteSuffix(fPlayer, fPlayer);
+        }
+
+        if (params.startsWith("fcolor")) {
+
+            String number = params.substring(params.lastIndexOf("_") + 1);
+            if (!StringUtils.isNumeric(number)) return null;
+
+            Map<Integer, String> colorsMap = new HashMap<>(fileFacade.message().format().fcolor().defaultColors());
+            if (params.startsWith("fcolor_out")) {
+                colorsMap.putAll(fPlayer.getFColors(FColor.Type.OUT));
+            } else if (params.startsWith("fcolor_see")) {
+                colorsMap.putAll(fPlayer.getFColors(FColor.Type.SEE));
+            } else {
+                colorsMap.putAll(fPlayer.getFColors(FColor.Type.SEE));
+                colorsMap.putAll(fPlayer.getFColors(FColor.Type.OUT));
+            }
+
+            return colorsMap.get(Integer.parseInt(number));
+        }
+
+        SettingText settingText = SettingText.fromString(params);
+        if (settingText != null) {
+            String value = fPlayer.getSetting(settingText);
+            if (settingText == SettingText.CHAT_NAME && value == null) return "default";
+
+            return StringUtils.defaultString(value);
+        }
+
+        try {
+            MessageType messageType = MessageType.valueOf(params.toUpperCase());
+            return String.valueOf(fPlayer.isSetting(messageType));
+        } catch (IllegalArgumentException ignored) {
+            // ignore exception
+        }
+
+        return switch (params) {
+            case "player" -> fPlayer.getName();
+            case "ip" -> fPlayer.getIp();
+            case "ping" -> String.valueOf(fPlayerService.getPing(fPlayer));
+            case "online" -> String.valueOf(platformServerAdapter.getOnlinePlayerCount());
+            case "tps" -> platformServerAdapter.getTPS();
+            default -> null;
+        };
+    }
+
+    @Pulse(priority = Event.Priority.LOW)
+    public Event onMessageFormattingEvent(MessageFormattingEvent event) {
+        MessageContext messageContext = event.context();
+        FEntity sender = messageContext.sender();
+        if (placeholderAPIModule.isModuleDisabledFor(sender)) return event;
+
+        FPlayer fReceiver = messageContext.receiver();
+        boolean isUserMessage = messageContext.isFlag(MessageFlag.USER_MESSAGE);
+        if (!permissionChecker.check(sender, placeholderAPIModule.permission().use()) && isUserMessage) return event;
+        if (!(sender instanceof FPlayer fPlayer)) return event;
+
+        String message = messageContext.message();
+
+        // switch parsing
+        if (!messageContext.isFlag(MessageFlag.SENDER_INTEGRATION_PLACEHOLDERS)) {
+            FPlayer tempFPlayer = fPlayer;
+            fPlayer = fReceiver;
+            fReceiver = tempFPlayer;
+        }
+
+        try {
+            Universe universe = Universe.get();
+            if (universe != null) {
+                PlayerRef offlinePlayer = universe.getPlayer(fPlayer.getUuid());
+                message = PlaceholderAPI.setPlaceholders(offlinePlayer, message);
+
+                if (fPlayer.isOnline()) {
+                    PlayerRef receiver = universe.getPlayer(fReceiver.getUuid());
+                    if (receiver == null) {
+                        receiver = offlinePlayer;
+                    }
+
+                    message = PlaceholderAPI.setRelationalPlaceholders(offlinePlayer, receiver, message);
+                }
+            }
+
+        } catch (Exception ignored) {
+            // ignore placeholderapi exceptions
+        }
+
+        return event.withContext(messageContext.withMessage(message));
+    }
+
+}
