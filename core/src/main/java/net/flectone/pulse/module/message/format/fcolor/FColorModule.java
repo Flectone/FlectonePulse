c4c6ccba5be36aa4c30902dbe458ba351c9a9b56
TheFaser <artem.mochalin11@gmail.com>
2026-02-11 17:46:34 +0500

    refactor(fcolor): optimize color processing logic

--- core/src/main/java/net/flectone/pulse/module/message/format/fcolor/FColorModule.java
+++ core/src/main/java/net/flectone/pulse/module/message/format/fcolor/FColorModule.java
@@ -16,14 +16,13 @@ import net.flectone.pulse.module.message.format.convertor.LegacyColorConvertor;
 import net.flectone.pulse.module.message.format.fcolor.listener.FColorPulseListener;
 import net.flectone.pulse.platform.registry.ListenerRegistry;
 import net.flectone.pulse.processing.context.MessageContext;
-import net.flectone.pulse.util.file.FileFacade;
 import net.flectone.pulse.util.checker.PermissionChecker;
 import net.flectone.pulse.util.constant.MessageFlag;
-import net.kyori.adventure.text.Component;
+import net.flectone.pulse.util.file.FileFacade;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import org.apache.commons.lang3.RegExUtils;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.OptionalInt;
 
@@ -73,23 +72,7 @@ public class FColorModule extends AbstractModule {
         FPlayer receiver = messageContext.receiver();
         if (isModuleDisabledFor(receiver)) return messageContext;
 
-        // default map colors
-        Map<Integer, String> colorsMap = new HashMap<>(config().defaultColors());
-
-        // receivers see colors
-        updateColorsMap(colorsMap, receiver, FColor.Type.SEE);
-
-        // send out colors
-        if (messageContext.isFlag(MessageFlag.SENDER_COLOR_OUT)) {
-            if (sender instanceof FPlayer fPlayer) {
-                updateColorsMap(colorsMap, fPlayer, FColor.Type.OUT);
-            }
-        } else {
-            updateColorsMap(colorsMap, receiver, FColor.Type.OUT);
-        }
-
-        // convert legacy colors
-        colorsMap.forEach((integer, string) -> colorsMap.put(integer, legacyColorConvertor.convert(string)));
+        boolean isSenderColorOut = messageContext.isFlag(MessageFlag.SENDER_COLOR_OUT);
 
         messageContext = messageContext.addTagResolver(MessagePipeline.ReplacementTag.FCOLOR, (argumentQueue, context) -> {
             if (!argumentQueue.hasNext()) return MessagePipeline.ReplacementTag.emptyTag();
@@ -97,7 +80,21 @@ public class FColorModule extends AbstractModule {
             OptionalInt number = argumentQueue.pop().asInt();
             if (number.isEmpty()) return MessagePipeline.ReplacementTag.emptyTag();
 
-            return Tag.preProcessParsed(colorsMap.getOrDefault(number.getAsInt(), ""));
+            int index = number.getAsInt();
+            String color = getFColorOrDefault(receiver, FColor.Type.SEE, index, config().defaultColors().get(index));
+
+            // send out colors
+            if (isSenderColorOut) {
+                if (sender instanceof FPlayer fPlayer) {
+                    color = getFColorOrDefault(fPlayer, FColor.Type.OUT, index, color);
+                }
+            } else {
+                color = getFColorOrDefault(receiver, FColor.Type.OUT, index, color);
+            }
+
+            color = legacyColorConvertor.convert(StringUtils.defaultString(color));
+
+            return Tag.preProcessParsed(color);
         });
 
         // replace deprecated tag
@@ -108,9 +105,12 @@ public class FColorModule extends AbstractModule {
         return messageContext;
     }
 
-    private void updateColorsMap(Map<Integer, String> colorsMap, FPlayer fPlayer, FColor.Type type) {
+    private String getFColorOrDefault(FPlayer fPlayer, FColor.Type type, int index, String defaultColor) {
         if (permissionChecker.check(fPlayer, permission().colors().get(type))) {
-            colorsMap.putAll(fPlayer.getFColors(type));
+            Map<Integer, String> colorMap = fPlayer.getFColors(type);
+            return colorMap.getOrDefault(index, defaultColor);
         }
+
+        return defaultColor;
     }
 }
