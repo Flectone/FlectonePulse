f80ac5f135880486928df49899eedde8c5d44d11
TheFaser <artem.mochalin11@gmail.com>
2026-04-18 03:46:06 +0500

    feat(bungeecord): add login state tracking system

---
+++ minecraft/bungeecord/src/main/java/net/flectone/pulse/BungeeLoginStateListener.java
@@ -0,0 +1,53 @@
+package net.flectone.pulse;
+
+
+import net.md_5.bungee.api.event.*;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class BungeeLoginStateListener implements Listener {
+
+    private final Map<UUID, LoginStatus> loginStates = new HashMap<>();
+
+    public enum LoginStatus {
+        PRE_LOGIN, LOGIN, POST_LOGIN, CONNECTED
+    }
+
+    public LoginStatus getLoginStatus(UUID uuid) {
+        return loginStates.get(uuid);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPreLogin(PreLoginEvent event) {
+        loginStates.put(event.getConnection().getUniqueId(), LoginStatus.PRE_LOGIN);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onLogin(LoginEvent event) {
+        UUID uuid = event.getConnection().getUniqueId();
+        loginStates.put(uuid, LoginStatus.LOGIN);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPostLogin(PostLoginEvent event) {
+        UUID uuid = event.getPlayer().getUniqueId();
+        loginStates.put(uuid, LoginStatus.POST_LOGIN);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onServerConnected(ServerConnectedEvent event) {
+        UUID uuid = event.getPlayer().getUniqueId();
+        loginStates.put(uuid, LoginStatus.CONNECTED);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerDisconnect(PlayerDisconnectEvent event) {
+        UUID uuid = event.getPlayer().getUniqueId();
+        loginStates.remove(uuid);
+    }
+}
