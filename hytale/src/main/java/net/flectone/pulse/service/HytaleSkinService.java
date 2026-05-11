52b64aac86561b51176f3962b94beb9ec7168781
TheFaser <artem.mochalin11@gmail.com>
2026-05-11 19:12:20 +0500

    feat(hytale): add avatar and body skin support

--- hytale/src/main/java/net/flectone/pulse/service/HytaleSkinService.java
+++ hytale/src/main/java/net/flectone/pulse/service/HytaleSkinService.java
@@ -4,24 +4,28 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 import net.flectone.pulse.model.entity.FEntity;
+import net.flectone.pulse.util.file.FileFacade;
+import org.apache.commons.lang3.Strings;
 
 @Singleton
 @RequiredArgsConstructor(onConstructor = @__(@Inject))
 public class HytaleSkinService implements SkinService {
 
-    @Override
-    public String getSkin(FEntity fPlayer) {
-        return "";
-    }
+    private final FileFacade fileFacade;
 
     @Override
     public String getAvatarUrl(FEntity entity) {
-        return "";
+        return Strings.CS.replace(fileFacade.integration().avatarApiUrl(), "<skin>", getSkin(entity));
     }
 
     @Override
     public String getBodyUrl(FEntity entity) {
-        return "";
+        return Strings.CS.replace(fileFacade.integration().bodyApiUrl(), "<skin>", getSkin(entity));
+    }
+
+    @Override
+    public String getSkin(FEntity entity) {
+        return entity.uuid().toString();
     }
 
 }
