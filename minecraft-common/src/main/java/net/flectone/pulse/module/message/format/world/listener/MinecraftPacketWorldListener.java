35538f334f0bf97f9e59b8b281fd80ee9ac0e1ba
TheFaser <artem.mochalin11@gmail.com>
2026-08-10 08:15:24 +0500

    fix(world): update world only if player true teleported

--- minecraft-common/src/main/java/net/flectone/pulse/module/message/format/world/listener/MinecraftPacketWorldListener.java
+++ minecraft-common/src/main/java/net/flectone/pulse/module/message/format/world/listener/MinecraftPacketWorldListener.java
@@ -1,8 +1,9 @@
 package net.flectone.pulse.module.message.format.world.listener;
 
 import com.github.retrooper.packetevents.event.PacketListener;
-import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerPlayerPositionAndLook;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +19,15 @@ public class MinecraftPacketWorldListener implements PacketListener {
     private final FPlayerService fPlayerService;
 
     @Override
-    public void onPacketReceive(PacketReceiveEvent event) {
-        if (event.getPacketType() != PacketType.Play.Client.TELEPORT_CONFIRM) return;
+    public void onPacketSend(PacketSendEvent event) {
+        if (event.getPacketType() != PacketType.Play.Server.PLAYER_POSITION_AND_LOOK) return;
 
-        FPlayer fPlayer = fPlayerService.getFPlayer(event.getUser().getUUID());
-        if (fPlayer.isUnknown()) return;
+        WrapperPlayServerPlayerPositionAndLook wrapper = new WrapperPlayServerPlayerPositionAndLook(event);
+        if (wrapper.getRelativeFlags().getFullMask() != 0) {
+            FPlayer fPlayer = fPlayerService.getFPlayer(event.getUser().getUUID());
+            if (fPlayer.isUnknown()) return;
 
-        worldModule.update(fPlayer);
+            worldModule.update(fPlayer);
+        }
     }
 }
