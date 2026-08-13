2db10d3b2dc25a3ecef3fc6b2cc8165948a6c9fb
TheFaser <artem.mochalin11@gmail.com>
2026-08-13 21:43:08 +0500

    fix(command): update parser implementations

--- minecraft-common/src/main/java/net/flectone/pulse/platform/registry/BrigadierCommandRegistry.java
+++ minecraft-common/src/main/java/net/flectone/pulse/platform/registry/BrigadierCommandRegistry.java
@@ -3,10 +3,10 @@ package net.flectone.pulse.platform.registry;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import io.leangen.geantyref.TypeToken;
 import net.flectone.pulse.model.entity.FPlayer;
-import net.flectone.pulse.parser.integer.ColorParser;
-import net.flectone.pulse.parser.integer.DurationReasonParser;
-import net.flectone.pulse.parser.player.PlayerParser;
-import net.flectone.pulse.parser.string.MessageParser;
+import net.flectone.pulse.parser.integer.ColorParserImpl;
+import net.flectone.pulse.parser.integer.DurationReasonParserImpl;
+import net.flectone.pulse.parser.player.PlayerParserImpl;
+import net.flectone.pulse.parser.string.MessageParserImpl;
 import org.incendo.cloud.brigadier.BrigadierSetting;
 import org.incendo.cloud.brigadier.CloudBrigadierManager;
 import org.incendo.cloud.parser.standard.StringParser;
@@ -16,22 +16,22 @@ public interface BrigadierCommandRegistry extends CommandRegistry {
     default void setupBrigadierManager(CloudBrigadierManager<FPlayer, ?> brigadierManager) {
         brigadierManager.setNativeSuggestions(new TypeToken<StringParser<FPlayer>>() {}, true);
 
-        brigadierManager.registerMapping(new TypeToken<PlayerParser>() {},
+        brigadierManager.registerMapping(new TypeToken<PlayerParserImpl>() {},
                 builder -> builder.cloudSuggestions()
                         .to(_ -> StringArgumentType.string())
         );
 
-        brigadierManager.registerMapping(new TypeToken<DurationReasonParser>() {},
+        brigadierManager.registerMapping(new TypeToken<DurationReasonParserImpl>() {},
                 builder -> builder.cloudSuggestions()
                         .to(_ -> StringArgumentType.greedyString())
         );
 
-        brigadierManager.registerMapping(new TypeToken<ColorParser>() {},
+        brigadierManager.registerMapping(new TypeToken<ColorParserImpl>() {},
                 builder -> builder.cloudSuggestions()
                         .to(_ -> StringArgumentType.greedyString())
         );
 
-        brigadierManager.registerMapping(new TypeToken<MessageParser>() {},
+        brigadierManager.registerMapping(new TypeToken<MessageParserImpl>() {},
                 builder -> builder.cloudSuggestions()
                         .to(_ -> StringArgumentType.greedyString())
         );
