1ed68d0f06ae4b035a812053d71dcc572e7d7482
TheFaser <artem.mochalin11@gmail.com>
2026-09-04 15:06:21 +0500

    refactor(scheduler): remove module chains clearing

--- common/src/main/java/net/flectone/pulse/scheduler/TaskSchedulerImpl.java
+++ common/src/main/java/net/flectone/pulse/scheduler/TaskSchedulerImpl.java
@@ -71,8 +71,6 @@ public class TaskSchedulerImpl implements TaskScheduler {
     public void shutdown() {
         disabled = true;
 
-        moduleChains.clear();
-
         ExecutorService currentExecutorService = executorService;
         if (currentExecutorService == null) {
             scheduledTasks.clear();
