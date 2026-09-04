3dd4e824efa4fc96f01ec33d0059e4a10f04d4d0
TheFaser <artem.mochalin11@gmail.com>
2026-09-04 13:13:21 +0500

    refactor(editor): improve Spark server initialization

--- common/src/main/java/net/flectone/pulse/module/command/flectonepulse/web/SparkServer.java
+++ common/src/main/java/net/flectone/pulse/module/command/flectonepulse/web/SparkServer.java
@@ -4,8 +4,11 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 import net.flectone.pulse.file.FileFacade;
+import net.flectone.pulse.logging.FLogger;
 import net.flectone.pulse.module.command.flectonepulse.web.controller.EditorController;
 import spark.Service;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 @Singleton
 @RequiredArgsConstructor(onConstructor = @__(@Inject))
@@ -13,6 +16,7 @@ public class SparkServer {
 
     private final FileFacade fileFacade;
     private final EditorController controller;
+    private final FLogger fLogger;
 
     private Service sparkService;
 
@@ -21,19 +25,35 @@ public class SparkServer {
     }
 
     public void onEnable() {
-        if (sparkService != null) {
-            sparkService.stop();
-        }
+        onDisable();
 
-        sparkService = Service.ignite();
-        sparkService.port(fileFacade.command().flectonepulse().editor().port());
-        sparkService.staticFiles.location("/");
+        int port = fileFacade.command().flectonepulse().editor().port();
 
-        sparkService.before((_, res) -> res.type("text/html; charset=utf-8"));
+        Service service = Service.ignite();
+
+        AtomicReference<Exception> initException = new AtomicReference<>();
+        service.initExceptionHandler(initException::set);
+
+        service.port(port);
+        service.staticFiles.location("/");
+
+        service.before((_, res) -> res.type("text/html; charset=utf-8"));
 
         controller.initConfigFiles();
-        controller.setupRoutes(sparkService);
-        sparkService.init();
+        controller.setupRoutes(service);
+
+        service.init();
+        service.awaitInitialization();
+
+        Exception exception = initException.get();
+        if (exception != null) {
+            service.stop();
+
+            fLogger.warning("Editor did not start on port %s", exception, port);
+            return;
+        }
+
+        sparkService = service;
     }
 
     public void onDisable() {
